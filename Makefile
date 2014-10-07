b7147f4ab9c9451ea052276431d35ef3ad8bebbd
Borje Granberg <borje.granberg@gmail.com>
2014-10-07 15:16:42 +0200

    Automatically generate tipuesearch_pages

--- Makefile
+++ Makefile
@@ -1,7 +1,14 @@
 all:
 	hugo
+
 server:
 	hugo server --watch
+
 publish:
 	test -e /Volumes/Web/recept
+	hugo
+	./generateTipuePages.py public/recept
 	hugo -d /Volumes/Web/recept
+
+clean:
+	rm -rf public
---
+++ generateTipuePages.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+import argparse
+import os
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("path", help="Where the site is")
+args = parser.parse_args()
+
+tipuesearch_pages = "var tipuesearch_pages = "
+
+paths = []
+for root, dirs, files in os.walk(args.path):
+    for f in files:
+        if f == "index.html":
+            paths.append(os.path.join("/", os.path.relpath(root, "public")))
+
+tipuesearch_pages += str(paths).replace("'", "\"") + ";"
+
+output = ""
+outputfile = "static/tipuesearch/tipuesearch_set.js"
+for l in open(outputfile):
+    if l.find("tipuesearch_pages") >= 0:
+        output += tipuesearch_pages
+    else:
+        output += l
+
+f = open(outputfile, "w")
+f.write(output)
