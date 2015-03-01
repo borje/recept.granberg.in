2c06e89909683c021ef4ecad29d146f7d06576f4
Borje Granberg <borje.granberg@gmail.com>
2015-03-01 14:05:44 +0100

    Added gulp file to project

---
+++ gulpfile.js
@@ -0,0 +1,22 @@
+var gulp = require('gulp');
+
+var imageResize = require('gulp-image-resize');
+ 
+gulp.task('default', function () {
+  gulp.src('static/img/food/*')
+    .pipe(imageResize({ 
+      width : 300,
+      height : 275,
+      crop : true,
+      upscale : false,
+      format : 'jpeg'
+    })).on('error', errorHandler)
+    .pipe(gulp.dest('public/img/thumbnails/'));
+});
+
+// Handle the error
+function errorHandler (error) {
+  console.log(error.toString());
+  this.emit('end');
+}
+
